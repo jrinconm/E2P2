f7dabe7d4efe511dca201640a3614366dc0dee6b
Julian <julianrinconm@gmail.com>
2021-01-10 01:11:14 +0100

    Ya pinta de color

---
+++ ejercicio2/src/components/ComponenteCelda.vue
@@ -0,0 +1,28 @@
+<!-- Creo el template-->
+<template>
+    <!-- Creo una fila bootstrap -->
+    <div @mouseover.stop="actualizaColor" :class="color" class="celda">
+    </div>
+    <!-- Cierro la sección template -->
+</template>
+<script>
+export default {
+    // El nombre del componente
+    name: 'componente-celda',
+    // El array que recibo del padre
+    props: [ 'datos' ],
+    data: function(){
+        return {
+            color: ""
+        }
+    },
+    methods: {
+	actualizaColor: function (event){
+        this.color=this.datos;
+        }
+
+    }
+}
+</script>
+<style scoped>
+</style>
--- ejercicio2/src/js/main.js
+++ ejercicio2/src/js/main.js
@@ -1,4 +1,13 @@
 // Importamos los componentes
+import ComponenteCelda from '../components/ComponenteCelda.vue'
+// Importamos los componentes
+// NOTA: para que funcione con Webpack, hemos instalado el plugin "vue-loader"
+// Se instala con "npm install vue vue-loader vue-template-compiler css-loader"
+// Los ficheros package.json y webpack.config.js se han adaptado para ello en este ejemplo siguiendo https://vue-loader.vuejs.org/guide/#manual-setup
+
+
+// Registramos componente cargado
+Vue.component('componente-celda', ComponenteCelda)
 // NOTA: para que funcione con Webpack, hemos instalado el plugin "vue-loader"
 // Se instala con "npm install vue vue-loader vue-template-compiler css-loader"
 // Los ficheros package.json y webpack.config.js se han adaptado para ello en este ejemplo siguiendo https://vue-loader.vuejs.org/guide/#manual-setup
@@ -11,19 +20,19 @@ let app = new Vue({
 	el: '#app',	
 	data: {
 		colores: ["red","green","blue","yellow","purple","white"],
-		color: undefined,
+		color: "",
 	},
 	// Para sacar botones segun tipo
 	computed: {	
-
 	},
 	methods: {
 		eligeColor: function(colorElegido){
 			this.color=colorElegido;
 			console.log(this.color);
-		}	,
+		},
 		fila: function(numero)	{
 			return "fila" + numero;
-		}
+		},
+
 	}
 });
