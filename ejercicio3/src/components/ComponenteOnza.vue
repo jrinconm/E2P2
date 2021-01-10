8b1b632ba4beedb166561b2a379e252bd2056771
Julian <julianrinconm@gmail.com>
2021-01-10 13:39:54 +0100

    Creo la tableta

--- ejercicio3/src/components/ComponenteOnza.vue
+++ ejercicio3/src/components/ComponenteOnza.vue
@@ -1,14 +1,14 @@
 <!-- Creo el template-->
 <template>
         <!-- Creo un div de la lista ya filtrada -->
-        <div class="celda"></div>
+        <div class="celda" :class=data></div>
     <!-- Cierro la sección template -->
 </template>
 <script>
 export default {
     // El nombre del componente
     name: 'componente-onza',
-    props: [ 'color' ],
+    props: [ 'data' ],
     computed: {
 	},
 }
--- ejercicio3/src/js/main.js
+++ ejercicio3/src/js/main.js
@@ -11,9 +11,8 @@ let app = new Vue({
 	// Indicamos el ID del Div que contiene la APP Vue
 	el: '#app',	
 	data: {
-
+		color: "normal"
 	},
-
 	methods: {
 
 	}
