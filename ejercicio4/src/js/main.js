0b289a438a063ae232ccaa9aa3c6fe99d58b7f5a
Julian <julianrinconm@gmail.com>
2021-01-10 20:48:30 +0100

    Error de CORS

--- ejercicio4/src/js/main.js
+++ ejercicio4/src/js/main.js
@@ -3,16 +3,24 @@ let app = new Vue({
   // Indicamos el DIV donde esta nuestra APP Vue
   el: '#app',
   data: {
-    usuarios: []
+    usuarios: [],
+    info: null
   },
   /*
   Al ser montada la aplicacion de Vue 2 https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
   Hacemos una peticion GET a la URL. Al recibir la respuesta en formato JSON, simplemente mapeamos 
   dicha respuesta a la variable usuarios
   */
-  mounted: function () {
-    axios.get('https://jsonplaceholder.typicode.com/users').then(
-      response => (this.usuarios = response.data)
-    );
+  mounted: function () {  
+    let mensaje = {
+      "accion" : 'pedirClave'
+    }
+    console.log(mensaje);
+    axios.post("https://apuntesfpinformatica.es/DWEC/S4ND1EG0/ordenes.php", mensaje)
+    .then(result => this.info = result )
+    .catch(error => {
+      this.errorMessage = error.message;
+      console.error("There was an error!", error);
+    });
   }
 });
