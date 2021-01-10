00158f1606f64f8b81532469a04b3f3b22ec1f96
Julian <julianrinconm@gmail.com>
2021-01-10 13:30:22 +0100

    Creación basica con componente

---
+++ ejercicio3/src/components/ComponenteOnza.vue
@@ -0,0 +1,18 @@
+<!-- Creo el template-->
+<template>
+        <!-- Creo un div de la lista ya filtrada -->
+        <div class="celda"></div>
+    <!-- Cierro la sección template -->
+</template>
+<script>
+export default {
+    // El nombre del componente
+    name: 'componente-onza',
+    props: [ 'color' ],
+    computed: {
+	},
+}
+</script>
+<style scoped>
+
+</style>
---
+++ ejercicio3/src/js/main.js
@@ -0,0 +1,20 @@
+import ComponenteOnza from '../components/ComponenteOnza.vue'
+// Importamos los componentes
+// NOTA: para que funcione con Webpack, hemos instalado el plugin "vue-loader"
+// Se instala con "npm install vue vue-loader vue-template-compiler css-loader"
+// Los ficheros package.json y webpack.config.js se han adaptado para ello en este ejemplo siguiendo https://vue-loader.vuejs.org/guide/#manual-setup
+
+
+// Registramos componente cargado
+Vue.component('componente-onza', ComponenteOnza)
+let app = new Vue({
+	// Indicamos el ID del Div que contiene la APP Vue
+	el: '#app',	
+	data: {
+
+	},
+
+	methods: {
+
+	}
+});
