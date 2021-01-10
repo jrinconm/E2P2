02d13df4c2b90c7e9c57dc34b335af9e2ec0c141
Julian <julianrinconm@gmail.com>
2021-01-10 16:04:05 +0100

    Muestro texto

--- ejercicio3/src/components/ComponenteOnza.vue
+++ ejercicio3/src/components/ComponenteOnza.vue
@@ -1,15 +1,31 @@
 <!-- Creo el template-->
 <template>
         <!-- Creo un div de la lista ya filtrada -->
-        <div class="celda" :class=data></div>
+        <div @mouseleave.stop="fuera" @mouseover="dentro"  class="celda" :class="tipo">  
+        <p v-show="mostrar">¡MMMM...!</p>
+        </div>
     <!-- Cierro la sección template -->
 </template>
 <script>
 export default {
     // El nombre del componente
     name: 'componente-onza',
+    data: function(){
+        return {
+            mostrar: false,
+            tipo: this.data
+        }
+    },
     props: [ 'data' ],
-    computed: {
+    methods: {
+        dentro: function(){
+            this.mostrar=true;
+            this.tipo="seleccionado";
+        },
+        fuera: function(){
+            this.mostrar=false;
+            this.tipo="normal";
+        },
 	},
 }
 </script>
