2da70f5eadcbbba2c387a7d567dd89d759350374
Julian <julianrinconm@gmail.com>
2021-01-10 21:43:44 +0100

    Asignamos la clave al entry. Deshabilitamos el entry 1

--- ejercicio4/src/js/main.js
+++ ejercicio4/src/js/main.js
@@ -4,7 +4,25 @@ let app = new Vue({
   el: '#app',
   data: {
     usuarios: [],
+    clave: null,
     info: null
+  },
+  methods: {
+    pideclave: function(){
+      let mensaje = {
+        'tipoAtaque' : "DDOS",
+        'IPDestino' : "256.256.256.256",
+        'Clave' : "DBODBNVTB"
+      }
+      let formDa = new FormData();
+      formDa.append('accion', 'pedirClave');
+      axios.post("https://apuntesfpinformatica.es/DWEC/S4ND1EG0/ordenes.php",formDa)
+      .then(result => this.clave = result.data )
+      .catch(error => {
+        this.errorMessage = error.message;
+        console.error("There was an error!", error);
+      });
+    }
   },
   /*
   Al ser montada la aplicacion de Vue 2 https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
@@ -12,19 +30,6 @@ let app = new Vue({
   dicha respuesta a la variable usuarios
   */
   mounted: function () {  
-    let mensaje = {
-      'tipoAtaque' : "DDOS",
-      'IPDestino' : "256.256.256.256",
-      'Clave' : "DBODBNVTB"
-    }
-    let formDa = new FormData();
-    formDa.append('accion', 'pedirClave');
-    console.log(formDa);
-    axios.post("https://apuntesfpinformatica.es/DWEC/S4ND1EG0/ordenes.php",formDa)
-    .then(result => this.info = result )
-    .catch(error => {
-      this.errorMessage = error.message;
-      console.error("There was an error!", error);
-    });
+
   }
 });
